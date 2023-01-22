ca8f9c283220889634f69c8ca74f8e73d8a28cf7
Osvaldo Gago <opgago@yahoo.com>
2023-01-22 16:37:52 +0100

    First code

---
+++ Abrantes.js
@@ -0,0 +1,17 @@
+/* jshint esversion:6 */
+
+Abrantes = Object.create(null);
+
+Abrantes.variants = [
+    
+    // Original
+    function(){
+        console.log("Original");
+    },
+
+    // Variant A
+    function(){
+        console.log("Variant A");
+    },
+
+];
---
+++ MyTest.js
@@ -0,0 +1,17 @@
+/* jshint esversion:6 */
+
+var MyTest = Object.create(Abrantes);
+
+Abrantes.variants = [
+    
+    // Original
+    function(){
+        console.log("Original Mi test");
+    },
+
+    // Variant A
+    function(){
+        console.log("Variant Mi test");
+    },
+
+];
