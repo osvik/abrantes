ab21b961f956224409e9b7daf704ad1dd953ca65
Osvaldo Gago <opgago@yahoo.com>
2023-01-29 08:32:46 +0000

    Add defer example and jQuery

---
+++ examples/defer/MyDeferTest.js
@@ -0,0 +1,34 @@
+// Create the test
+var MyDeferTest = Object.create(Abrantes);
+
+// Define the variants
+MyDeferTest.variants = [
+
+    // Original
+    function () {
+        $("#bigH1").text("MyDeferTest Original");
+    },
+
+    // Variant 1
+    function () {
+        $("#bigH1").text("MyDeferTest Variant 1");
+    },
+
+    // Variant 2
+    function () {
+        $("#bigH1").text("MyDeferTest Variant 2");
+    },
+
+];
+
+// Assign a variant
+MyDeferTest.assignVariant("MyDeferTest");
+
+// Render the variant
+MyDeferTest.renderVariant();
+
+// Persist the variant in the browser
+MyDeferTest.persist("session");
+
+// Track the variant in a dimension 
+MyDeferTest.track("test_event");
---
+++ examples/defer/gtag-footer.js
@@ -0,0 +1,2 @@
+// Use the window.gtagParams in the gtag config and configure it in the header
+gtag('config', 'G-50HRM8825D', window.gtagParams);
