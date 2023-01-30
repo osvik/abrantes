fa272b1df0de8401dbeb4aee9b846fe0e26b42f1
Osvaldo Gago <opgago@yahoo.com>
2023-01-30 21:04:47 +0000

    Abrantes formatting

--- Abrantes.js
+++ Abrantes.js
@@ -9,12 +9,12 @@ Abrantes.variant = undefined;
  * Assigns a variant to a user
  * @param {string} testId 
  */
-Abrantes.assignVariant = function(testId){
-    if ( typeof(testId) !== "string" || testId.length === 0 ) {
-        throw("You need to provide an ID when assigning a variant");
+Abrantes.assignVariant = function (testId) {
+    if (typeof (testId) !== "string" || testId.length === 0) {
+        throw ("You need to provide an ID when assigning a variant");
     }
     this.testId = testId;
-    if ( typeof(this.readPersistent()) === "number" ) {
+    if (typeof (this.readPersistent()) === "number") {
         this.variant = this.readPersistent();
     } else {
         this.variant = this.randomVar();
@@ -25,25 +25,25 @@ Abrantes.assignVariant = function(testId){
  * Renders the variant and adds the class variant-x to the <body> tag
  * @param {Number} variant 
  */
-Abrantes.renderVariant = function(variant=this.variant){
-    if (typeof (this.variants[variant]) === "function" ) {
+Abrantes.renderVariant = function (variant = this.variant) {
+    if (typeof (this.variants[variant]) === "function") {
         this.variants[variant]();
-        document.getElementsByTagName("body")[0].classList.add( this.testId + "-" + variant); 
+        document.getElementsByTagName("body")[0].classList.add(this.testId + "-" + variant);
     } else {
-        throw("The variant " + variant + " does not exist");
+        throw ("The variant " + variant + " does not exist");
     }
 };
 
 /**
  * Stores the test id (object name) and variant in localStorage
  */
-Abrantes.persist = function(context){
-    if (context==="user") {
-        localStorage.setItem( this.testId, this.variant);
-    } else if (context==="session") {
-        sessionStorage.setItem( this.testId, this.variant);
+Abrantes.persist = function (context) {
+    if (context === "user") {
+        localStorage.setItem(this.testId, this.variant);
+    } else if (context === "session") {
+        sessionStorage.setItem(this.testId, this.variant);
     } else {
-        throw("You must use either 'user' or 'session'");
+        throw ("You must use either 'user' or 'session'");
     }
 };
 
@@ -51,15 +51,15 @@ Abrantes.persist = function(context){
  * Reads the value in the storage
  * @returns 
  */
-Abrantes.readPersistent = function() {
-    const sessionData = sessionStorage.getItem( this.testId);
-    if ( typeof(sessionData) === "string" || typeof(sessionData) === "number" ){
+Abrantes.readPersistent = function () {
+    const sessionData = sessionStorage.getItem(this.testId);
+    if (typeof (sessionData) === "string" || typeof (sessionData) === "number") {
         return Number(sessionData);
-    }  
-    const userData = localStorage.getItem( this.testId);
-    if (typeof(userData) === "string" || typeof(userData) === "number" ){
+    }
+    const userData = localStorage.getItem(this.testId);
+    if (typeof (userData) === "string" || typeof (userData) === "number") {
         return Number(userData);
-    } 
+    }
     return undefined;
 };
 
@@ -67,21 +67,21 @@ Abrantes.readPersistent = function() {
  * Object where the user can read and write the settings
  */
 Abrantes.settings = {
-    
+
 };
 
 /**
  * Array of functions with the changes of each variant. The variants are also defined in an object that extends Abrantes.
  */
 Abrantes.variants = [
-    
+
 ];
 
 /**
  * Selects a random variation from the list of available variants
  * @returns number
  */
-Abrantes.randomVar = function(){
+Abrantes.randomVar = function () {
     const numberVariants = this.variants.length;
     this.variant = Math.round(Math.random() * (numberVariants - 1));
     return this.variant;
@@ -91,11 +91,12 @@ Abrantes.randomVar = function(){
  * Track the user using Google Analytics 4 custom dimensions
  * @param {string} customDim The id of the custom dimension, created in Admin > Property > Custom definitions 
  */
-Abrantes.track = function(customDim){
-    if(typeof (window.gtagParams) !== "object"){
-        throw("window.gtagParams must be an object");
+Abrantes.track = function (customDim) {
+    if (typeof (window.gtagParams) !== "object") {
+        throw ("window.gtagParams must be an object");
     }
     let setObj = {};
     setObj[customDim] = this.testId + "-" + this.variant;
     Object.assign(window.gtagParams, setObj);
 };
+
