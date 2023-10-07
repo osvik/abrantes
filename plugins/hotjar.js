a56704835d503baab3d48b0aee8a2eaa67292cee
Osvaldo Gago <opgago@yahoo.com>
2023-10-07 23:00:56 +0100

    Custom events for cookie acceptance

--- plugins/hotjar.js
+++ plugins/hotjar.js
@@ -2,25 +2,41 @@
 {
     const hotjarEvent = {
 
+        settings: {
+            hotjar: {
+                triggerEvents: ["load", "cookies:accept", "cookies:acceptall", "cookies:ok"]
+            }
+        },
+
         /**
          * Triggers an event in Hotjar
          */
         hotjar: function () {
+
             if (this.variant === -1) {
                 return;
             }
+
             if (typeof (hj) === "function") {
                 hj('event', this.testId + "-" + this.variant);
                 return;
             }
+
             const self = this;
-            window.addEventListener("load", function () {
-                if (typeof (hj) === "function") {
-                    hj('event', self.testId + "-" + self.variant);
-                } else {
-                    console.error("Hotjar not found");
-                }
+            // Loops trough cookie aceptance events, defined in .settings.hotjar.triggerEvents
+            this.settings.hotjar.triggerEvents.forEach(function (ev) {
+                window.addEventListener(ev, function () {
+                    setTimeout(() => {
+                        if (typeof (hj) === "function") {
+                            hj('event', self.testId + "-" + self.variant);
+                        } else {
+                            console.info("Hotjar not present");
+                        }
+                    }, 2000);
+                });
             });
+
+            // FIXME Change the tracking script to fire the events
         }
 
     };
