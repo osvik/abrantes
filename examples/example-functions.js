9482a767a1c68e93d88efb9cf7c18d6306301e6b
Osvaldo Gago <opgago@yahoo.com>
2025-12-29 13:21:27 +0100

    Add example functions

--- examples/example-functions.js
+++ examples/example-functions.js
@@ -24,7 +24,44 @@ var variants = [
     */
     function () {
         document.querySelectorAll("h1").forEach((n) => {
-            n.classList.add("d-done");
+            n.classList.add("d-none");
+        });
+    },
+
+    /*
+    * Hide all h1 tags
+    */
+    function () {
+        document.querySelectorAll("h1").forEach((n) => {
+            n.style.display = "none";
+        });
+    },
+
+    /*
+    * Scroll to the second paragraph inside a div with class "content"
+    */
+    function () {
+        const contentDiv = document.querySelector("div.content");
+        if (contentDiv) {
+            const paragraphs = contentDiv.querySelectorAll("p");
+            if (paragraphs.length >= 2) {
+                paragraphs[1].scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    },
+
+    /*
+    * Replace the image with the source "example.jpg" by "example-variant.jpg"
+    */
+    function () {
+        document.querySelectorAll("img").forEach((n) => {
+            const original = "example.jpg";
+            const variant = "example-variant.jpg";
+            if (n.src.includes(original)) {
+                n.src = n.src.replace(original, variant);
+                // delete the srcset attribute to avoid loading the original image from there
+                n.removeAttribute("srcset");
+            }
         });
     },
 
