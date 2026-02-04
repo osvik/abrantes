b1e92af8f81b6f195fa40a04ddabd0de22129ee1
Osvaldo Gago <opgago@yahoo.com>
2026-02-04 19:40:57 +0100

    v1.4.0

--- AbrantesPlusMod.js
+++ AbrantesPlusMod.js
@@ -10,7 +10,7 @@ const Abrantes = Object.create(null);
 
 Abrantes.testId = undefined;
 Abrantes.variant = undefined;
-Abrantes.version = "1.3.1";
+Abrantes.version = "1.4.0";
 Abrantes.excludedVariantsForNewUsers = [];
 
 /**
